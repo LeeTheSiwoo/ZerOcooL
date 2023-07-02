7e94d24d3e4eb8fcd8ca7cec15967a090973f3bb
leethesiwoo <leethesiwoo@gmail.com>
2023-07-02 16:45:20 +0600

    sex

---
+++ modules/commands/adminv2.js
@@ -0,0 +1,23 @@
+// This File Create By Md Sagor Munshi.
+// Don't Change Credit.
+// You Change Credit...!!! Beacuse You are Mother Fucker.
+// Contact Me: fb.com/MdSagorMunshiXagor
+module.exports.config = {
+	name: "admin",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Sagor",
+	description: "Bot operator information",
+	commandCategory: "info",
+	cooldowns: 1
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage(`don" t
+--------------------------------------------
+FACEBOOK : L. SIWOO
+GENDER : Male
+Age : 18+
+Relationship : Single
+Work : Student, Programmer, IT Specialist, Gamer...!
+INSTA :@leesiwoo_s
+TELEGRAM : @leesiwoo_s`, event.threadID, event.messageID);
